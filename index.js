46ba93bf0bea4aa9199353de938ec94f251f2dee
lolwner <lolwner@gmail.com>
2020-04-23 00:21:40 +0300

    Added env variable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log(process.env);
-client.login('discord_token');
+client.login(process.env.discord_token);
 client.on('ready', () => {
 console.log('The bot is ready');
 });
