f26fe1048e64d884a6a87a6bcd8eca814e62d9bc
lolwner <lolwner@gmail.com>
2020-09-26 00:39:32 +0300

    Added voice channel listening

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.discord_token);
-client.on('ready', () => {
-console.log('The bot is ready');
-});
-client.on('message', (msg) => {
-if (msg.content.includes('bot')) {
-    msg.reply('Did you mention me?');
-}
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    const textChannel = newMember.guild.channels.cache.find(channel => channel.name === 'железка-пишет-тут')
+
+    if (oldMember.mute != newMember.mute || newMember.deaf != newMember.deaf) {
+        return;
+    }
+
+    if (newMember.channel && newMember.channel.name) {
+        var newUserChannel = newMember.channel.name;
+
+        if (newUserChannel == 'Попизделочная') {
+            textChannel.send(`<@${newMember.member.user.id}> has joined the channel`);
+        }
+    }
 });
